d07323e0aeb50e689ced956fe095abe0cb5edf66
msegovic <msegovic1003@gmail.com>
2018-08-21 09:58:19 +0200

    Changes

--- src/WebChecker.java
+++ src/WebChecker.java
@@ -100,8 +100,8 @@ public class WebChecker {
                     if (httpClient.succeededStatus == "OK") {
                         validLabel.setText("URL valid !");
                         statusLabel.setText("Status code: " + httpClient.succeededStatus);
-                        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
-                        textArea.append(output);}
+                        /*for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+                        textArea.append(output);}*/
                     } else {
                         validLabel.setText("URL valid !");
                         statusLabel.setText("Status code: " + httpClient.failedStatus);
